e29037cb1d5727b66dc1cbd6d29b26c4d431a5aa
Maxim <shabanov_maxim@mail.ru>
2026-03-31 07:31:38 +0300

    Поправлен базовая страница html, убраны ключи в файл env из настроек

--- languageflash/languageflash/settings.py
+++ languageflash/languageflash/settings.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
 
+
+import os
+
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5mt(gh29+ab8@3t5x+9iy5d6oxk+2a$ql%x)b1fi49-sz=vni5'
+SECRET_KEY = os.getenv('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='localhost').split(' ')
 
 
 # Application definition
